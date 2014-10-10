f874eb59149217b5f2a28ffd8b58e482970c32b0
icchy <r.icchy.m4cc3r@gmail.com>
2014-10-10 17:09:18 +0900

    change package name

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-package test;
+package something_adhoc;
 
 public class Hello {
 	public static void main(String[] args) {
--- TestClient.java
+++ TestClient.java
@@ -1,4 +1,4 @@
-package sensin;
+package something_adhoc;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
